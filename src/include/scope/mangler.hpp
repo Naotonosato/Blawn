a52ff4fb2e6ecda363258262d59eb72edadafaa2
Naoto Uehara <nawonaototomousu@gmail.com>
2020-05-23 12:28:20 +0900

    dev now

--- src/include/scope/mangler.hpp
+++ src/include/scope/mangler.hpp
@@ -4,7 +4,6 @@
 namespace scope
 {
 class Scope;
-
 std::string mangle(const std::string& name, const ScopeID scope);
 std::pair<std::string, ScopeID> demangle(const std::string& mangled_name);
 }  // namespace scope
